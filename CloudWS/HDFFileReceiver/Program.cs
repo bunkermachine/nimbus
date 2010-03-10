b5a077b4a17e1b62f5e45d1d7e23cb6276df4589
David Li <davidli@cs.stanford.edu>
2010-03-10 10:55:33 -0800

    Testing output reading

--- CloudWS/HDFFileReceiver/Program.cs
+++ CloudWS/HDFFileReceiver/Program.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Diagnostics;
+using System.IO;
 
 namespace HDFFileReceiver
 {
     public class Program
     {
-        public void parseHDF(String hdfFileName)
+        public StreamReader parseHDF(String hdfFileName)
         {
+            char[] data;
+            data = new char[1000];
+     
             Process process = new Process();
-            process.StartInfo.FileName = "C:\\RdHdf.exe";
-            process.StartInfo.Arguments = hdfFileName;
+            process.StartInfo.FileName = "notepad.exe";
+            process.StartInfo.Arguments = "RdHDF.m";
+            process.StartInfo.RedirectStandardOutput = true;
             try
             {
                 process.Start();
@@ -28,6 +33,8 @@ namespace HDFFileReceiver
                 Console.WriteLine("");
 
             }
+
+            return process.StandardOutput;
         }
     }
 }
