5afcdabe062b00bc4ccb1130417564e518d040c1
Sudarshan <tr.sudarshan@gmail.com>
2010-05-01 13:27:43 -0700

    Code for table operations. Buggy.

---
+++ SimpleTableSample/WebRole1/ContactDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace WebRole1
+{
+    public class ContactDataModel : TableServiceEntity
+    {
+         public ContactDataModel(string partitionKey, string rowKey)
+        : base(partitionKey, rowKey)
+    {
+    }
+
+    public ContactDataModel(): this(Guid.NewGuid().ToString(), String.Empty)
+    {
+    }
+
+    public string Name { get; set; }
+    public string Address { get; set; }
+
+    }
+}
