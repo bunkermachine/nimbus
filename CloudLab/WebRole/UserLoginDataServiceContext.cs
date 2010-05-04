a021112e37834e9479d7d1f546c391c3935c5e10
Sudarshan <tr.sudarshan@gmail.com>
2010-05-04 10:45:35 -0700

    Including login based work to master code

---
+++ CloudLab/WebRole/UserLoginDataServiceContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace WebRole
+{
+    public class UserLoginDataServiceContext : TableServiceContext
+    {
+        public UserLoginDataServiceContext(string baseAddress, StorageCredentials credentials)
+            : base(baseAddress, credentials)
+        {
+        }
+
+        public const string UserLoginTableName = "UserLoginTable";
+
+        public IQueryable<UserLoginDataModel> UserLoginTable
+        {
+            get
+            {
+                return this.CreateQuery<UserLoginDataModel>(UserLoginTableName);
+            }
+        }
+
+    }
+}
---
+++ CloudLab/WebRole/UserLoginDataSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System.Data.Services.Client;
+
+namespace WebRole
+{
+    public class UserLoginDataSource
+    {
+        private UserLoginDataServiceContext _ServiceContext = null;
+
+        public UserLoginDataSource()
+        {
+            var storageAccount = CloudStorageAccount.FromConfigurationSetting("DataConnectionString");
+            _ServiceContext = new UserLoginDataServiceContext(storageAccount.TableEndpoint.ToString(), storageAccount.Credentials);
+
+            // Create the tables
+            // In this case, just a single table.  
+            storageAccount.CreateCloudTableClient().CreateTableIfNotExist(UserLoginDataServiceContext.UserLoginTableName);
+
+        }
+
+        public IEnumerable<UserLoginDataModel> Select()
+        {
+            var results = from xp in _ServiceContext.UserLoginTable
+                          select xp;
+
+            var query = results.AsTableServiceQuery<UserLoginDataModel>();
+            var queryResults = query.Execute();
+
+            return queryResults;
+        }
+
+        public void Delete(UserLoginDataModel itemToDelete)
+        {
+            _ServiceContext.AttachTo(UserLoginDataServiceContext.UserLoginTableName, itemToDelete, "*");
+            _ServiceContext.DeleteObject(itemToDelete);
+            _ServiceContext.SaveChanges();
+        }
+
+        public void Insert(UserLoginDataModel newItem)
+        {
+            _ServiceContext.AddObject(UserLoginDataServiceContext.UserLoginTableName, newItem);
+            _ServiceContext.SaveChanges();
+        }
+    }
+}
