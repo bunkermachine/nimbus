c09e0f5eeca0fcfc3c2c2713372b009ed713bc81
Sudarshan <tr.sudarshan@gmail.com>
2010-05-16 11:22:07 -0700

    Abstracted Project and Task classes from UserStatus class.

---
+++ CloudLab/CloudLab.Common/Project.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace CloudLab.Common
+{
+    class Project
+    {
+        private string projectName;
+        private string projectDummyFile;
+        private Dictionary<string, Task> tasksMap;
+        private Dictionary<string, string> projectMetadata;
+        private CloudBlob projectDummyFileBlob;
+
+        public Project(string projectName, string projectDummyFile, CloudBlob projectDummyFileBlob)
+        {
+            setProjectName(projectName);
+            setProjectDummyFile(projectDummyFile);
+            setProjectDummyFileBlob(projectDummyFileBlob);
+        }
+
+        protected void setProjectDummyFile(string projectDummyFile)
+        {
+            this.projectDummyFile = projectDummyFile;
+        }
+
+        protected string getProjectDummyFile()
+        {
+            return this.projectDummyFile;
+        }
+
+        protected void setProjectDummyFileBlob(CloudBlob projectDummyFileBlob)
+        {
+            this.projectDummyFileBlob = projectDummyFileBlob;
+        }
+
+        protected CloudBlob getProjectDummyFileBlob()
+        {
+            return this.projectDummyFileBlob;
+        }
+
+        protected string getProjectName()
+        {
+            return this.projectName;
+        }
+
+        protected void setProjectName(string projectName)
+        {
+            this.projectName = projectName;
+        }
+
+        protected Dictionary<string, Task>.ValueCollection getTasksList()
+        {
+            return this.tasksMap.Values;
+        }
+
+        protected void addTaskToCurrentProject(string taskName, Task task)
+        {
+            this.tasksMap.Add(taskName, task);
+        }
+
+        protected bool isThisTaskUnderCurrentProject(string taskName)
+        {
+            return this.tasksMap.ContainsKey(taskName);
+        }
+
+        protected Task getTaskFromCurrentProject(string taskName)
+        {
+            return this.tasksMap[taskName];
+        }
+
+        protected void addProjectMetadata(string propertyName, string propertyValue)
+        {
+            this.projectMetadata.Add(propertyName, propertyValue);
+        }
+
+        protected bool isThisPropertySetInProjectMetadata(string propertyName)
+        {
+            return this.projectMetadata.ContainsKey(propertyName);
+        }
+
+        protected string getContainerMetadataValue(string propertyName)
+        {
+            return this.projectDummyFileBlob.Metadata[propertyName];
+        }
+
+        protected void commitProjectMetadata()
+        {
+            NameValueCollection prevProjectMetaData = this.projectDummyFileBlob.Metadata;
+
+            foreach (string property in prevProjectMetaData.Keys)
+            {
+                this.projectDummyFileBlob.Metadata.Add(property, prevProjectMetaData[property]);
+            }
+
+            foreach (string property in this.projectMetadata.Keys)
+            {
+                this.projectDummyFileBlob.Metadata.Add(property, this.projectMetadata[property]);
+            }
+
+            this.projectDummyFileBlob.SetMetadata();
+
+            foreach (string property in this.projectMetadata.Keys)
+            {
+                this.projectMetadata.Remove(property);
+            }
+        }
+
+    }
+}
---
+++ CloudLab/CloudLab.Common/Task.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace CloudLab.Common
+{
+    class Task
+    {
+        private string taskName;
+        private string taskDummyFile;
+        private List<string> exes;
+        private List<string> dataFiles;
+        private Dictionary<string, string> taskMetadata;
+        private CloudBlob taskDummyFileBlob;
+
+        public Task(string taskName)
+        {
+            setTaskName(taskName);
+        }
+
+        protected string getTaskDummyFile()
+        {
+            return taskDummyFile;
+        }
+
+        protected void setTaskDummyFile(string taskDummyFile)
+        {
+            this.taskDummyFile = taskDummyFile;
+        }
+
+        protected void setTaskDummyFileBlob(CloudBlob taskDummyFileBlob)
+        {
+            this.taskDummyFileBlob = taskDummyFileBlob;
+        }
+
+        protected CloudBlob getTaskDummyFileBlob()
+        {
+            return this.taskDummyFileBlob;
+        }
+
+        protected string getTaskName()
+        {
+            return this.taskName;
+        }
+
+        protected void setTaskName(string taskName)
+        {
+            this.taskName = taskName;
+        }
+
+        protected List<string> getExes()
+        {
+            return this.exes;
+        }
+
+        protected List<string> getDataFiles()
+        {
+            return this.dataFiles;
+        }
+
+        protected void addTaskMetadata(string propertyName, string propertyValue)
+        {
+            this.taskMetadata.Add(propertyName, propertyValue);
+        }
+
+        protected bool isThisPropertySetInTaskMetadata(string propertyName)
+        {
+            return this.taskMetadata.ContainsKey(propertyName);
+        }
+
+        protected string getTaskMetadataValue(string propertyName)
+        {
+            return this.taskDummyFileBlob.Metadata[propertyName];
+        }
+
+        protected void commitTaskMetadata()
+        {
+            NameValueCollection prevTaskMetaData = this.taskDummyFileBlob.Metadata;
+
+            foreach (string property in prevTaskMetaData.Keys)
+            {
+                this.taskDummyFileBlob.Metadata.Add(property, prevTaskMetaData[property]);
+            }
+
+            foreach (string property in this.taskMetadata.Keys)
+            {
+                this.taskDummyFileBlob.Metadata.Add(property, this.taskMetadata[property]);
+            }
+
+            this.taskDummyFileBlob.SetMetadata();
+
+            foreach (string property in this.taskMetadata.Keys)
+            {
+                this.taskMetadata.Remove(property);
+            }
+        }
+    }
+}
